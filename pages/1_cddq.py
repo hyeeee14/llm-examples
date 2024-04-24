0d2d47f662bcec396bca3fcd0cf3553dc6c018ab
hyeeee14 <hyeeee14@gmail.com>
2024-04-24 23:45:29 +0900

    240424

--- pages/1_cddq.py
+++ pages/1_cddq.py
@@ -1,1 +1,6 @@
-import streamlit
+import streamlit as st
+
+with st.sidebar:
+    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
+
+st.title("📝 CDDQ 측정")
