d4148cd3a12b256bbe81d82d98f2371a811d4b5c
hyeeee14 <hyeeee14@gmail.com>
2024-04-25 12:49:30 +0900

    240425

--- pages/1_CDD Questionnaire.py
+++ pages/1_CDD Questionnaire.py
@@ -7,4 +7,4 @@ score_1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 q_1 = st.radio('1. I know that I have to choose a career, but I don’t have the motivation to make the decision now (I don’t feel like it)', options = score_1)   
 
 # Create Radio Buttons
-st.radio(label = 'Radio buttons', options = ['5점', '4점', '2점', '1점']
+st.radio(label = 'Radio buttons', options = ['5점', '4점', '2점', '1점'])
