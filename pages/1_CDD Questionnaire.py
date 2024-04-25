0819800c11da938cfda259bdc8ed64a058c010b6
hyeeee14 <hyeeee14@gmail.com>
2024-04-25 12:55:13 +0900

    240425

--- pages/1_CDD Questionnaire.py
+++ pages/1_CDD Questionnaire.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
 st.title("📝 Career Decision-making Difficulties Questionnaire")
-st.text("총 10개의 질문으로 진로결정 어려움에 대해 측정합니다. by. GAti")
+st.caption("Measure the difficulty of making career decisions with a total of 10 questions. by. Gati")
+
+# CSS를 사용하여 라디오 버튼을 수평으로 배치
+st.markdown(
+    """
+    <style>
+        div.row-widget.stRadio > div{flex-direction:row;}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 score_1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 q_1 = st.radio('1. I know that I have to choose a career, but I don’t have the motivation to make the decision now (I don’t feel like it)', options = score_1)   
