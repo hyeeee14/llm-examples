590e4a6896462eb65e813e919ae6cb1f029ddb63
hyeeee14 <hyeeee14@gmail.com>
2024-04-25 12:59:52 +0900

    240425

--- pages/1_CDD Questionnaire.py
+++ pages/1_CDD Questionnaire.py
@@ -19,3 +19,7 @@ q_1 = st.radio('1. I know that I have to choose a career, but I don’t have the
 
 score_2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 q_2 = st.radio('2. Work is not the most important thing in one’s life and therefore the issue of choosing a career doesn’t worry me much.', options = score_2)   
+
+with st.sidebar:
+    st.sidebar.header('Career Decision-making Difficulties Questionnaire')
+    st.sidebar.markdown('겪고 있는 진로 결정 어려움을 파악하기')
