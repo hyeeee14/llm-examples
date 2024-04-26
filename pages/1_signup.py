9a6b64e66a2e458be3a740b5060f37c2eea52ad2
hyeeee14 <hyeeee14@gmail.com>
2024-04-26 16:18:04 +0900

    240426

--- pages/1_signup.py
+++ pages/1_signup.py
@@ -2,7 +2,7 @@ import streamlit as st
 from st_supabase_connection import SupabaseConnection
 
 
-st_supabase_client = st.connection("supabase", type=SupabaseConnection)
+st_supabase_client = st.connection("supabase",type=SupabaseConnection, url=st.secrets['SUPABASE_URL'], key=st.secrets['SUPABASE_KEY'])
 
 
 with st.container(border=True):
