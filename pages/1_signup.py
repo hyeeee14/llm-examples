09104b59a2fd1a0b164d7906d03d8767fb0ce806
hyeeee14 <hyeeee14@gmail.com>
2024-04-30 16:17:02 +0900

    240430

--- pages/1_signup.py
+++ pages/1_signup.py
@@ -27,10 +27,10 @@ with st.container(border=True):
                         }
                     }
                 })
-                st.success("회원가입 성공! 로그인 페이지로 이동하세요.")
+                st.success("회원가입 성공. 로그인 페이지로 이동하세요.")
                                 
             except Exception as e:
-                st.error("회원가입 실패!")
+                st.error("회원가입 실패")
                 
             if email and password:
                 try:
