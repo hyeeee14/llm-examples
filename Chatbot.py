4f0347fc897322acf10215d37bf5c4fb15c9f3a4
Hyerim Han <105032106+hyeeee14@users.noreply.github.com>
2025-01-31 13:47:35 +0000

    set_page

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Choose a chatbot💬",
+    page_icon="✅",
+    layout="centered",
+    initial_sidebar_state="collapsed"  # 'collapsed'로 설정하면 사이드바 접힌 상태로 시작
+)
+
 st.markdown("실험에 참여해주셔서 감사합니다!📝")
 st.caption("챗봇🤖을 골라주세요.")
 
