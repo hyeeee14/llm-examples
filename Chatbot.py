cf1146c3f5b0f4001e0d00c8e11fc18fe6f12a16
Hyerim Han <105032106+hyeeee14@users.noreply.github.com>
2025-01-31 13:39:42 +0000

    250131 index=none

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.caption("실험에 참여해주셔서 감사합니다!📝")
-st.session_state["radio"] = st.radio("챗봇을 골라주세요!", ["chatbot_s", "chatbot_n"])
+st.session_state["radio"] = st.radio("챗봇을 골라주세요!", ["chatbot_s", "chatbot_n"], index=None)
 
 if st.session_state["radio"] == "chatbot_s":
     st.switch_page("pages/1_Chatbot-S.py")
