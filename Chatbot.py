279036b2255f7eb3d54c52492fba621bde24c1d8
Hyerim Han <105032106+hyeeee14@users.noreply.github.com>
2025-01-31 13:46:01 +0000

    h3, h1

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.markdown(
+    "<h3 style='text-align: center;'>실험에 참여해주셔서 감사합니다!📝</h1>",
+    "<h1 style='text-align: center;'>챗봇🤖을 골라주세요. </h1>",
+    unsafe_allow_html=True
+)
+
 st.markdown("실험에 참여해주셔서 감사합니다!📝")
 st.caption("챗봇을 골라주세요.")
 
