16583109bf71528adaf9f43b8717ce85df0e64e4
Hyerim Han <105032106+hyeeee14@users.noreply.github.com>
2025-01-31 13:16:22 +0000

    radio button

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.session_state["radio"] = st.radio("Radio", ["chatbot_s", "chatbot_n"])
+st.caption("실험에 참여해주셔서 감사합니다!📝")
+st.session_state["radio"] = st.radio("챗봇을 골라주세요!", ["chatbot_s", "chatbot_n"])
 
 if st.session_state["radio"] == "chatbot_s":
     st.switch_page("pages/1_Chatbot-S.py")
