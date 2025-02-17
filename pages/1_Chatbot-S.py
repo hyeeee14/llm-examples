5182293c0ab8d44147272ac19fd595a352e4d37e
Hyerim Han <105032106+hyeeee14@users.noreply.github.com>
2025-02-17 17:53:00 +0900

    Update 1_Chatbot-S.py

--- pages/1_Chatbot-S.py
+++ pages/1_Chatbot-S.py
@@ -1,41 +1,11 @@
-from openai import OpenAI
-import os
-#from langchain_chroma import Chroma
-from langchain_community.vectorstores import Chroma
-
-import pandas as pd
-import numpy as np
-import pickle
 import streamlit as st
-import json
-from langchain import hub
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.chains import RetrievalQA
-from langchain_openai import OpenAI
-import openai
-#import chromadb
-from operator import itemgetter
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain_core.prompts.prompt import PromptTemplate
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain_core.chat_history import InMemoryChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, AIMessage
+import os, json, chardet, openai
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain_core.tools import tool
-import chardet
+from langchain.memory import ConversationSummaryBufferMemory, 
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 
 # ----------------------------------------------------------------------------------------------------
 # streamlit
@@ -55,10 +25,6 @@ st.caption("🚀 AI Career Counselor Conversational Assistant produced by Hyerim
 
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 OPENAI_ORGANIZATION = st.secrets['OPENAI_ORGANIZATION']
-
-# Set OpenAI API key 
-client = OpenAI(api_key=OPENAI_API_KEY, 
-                organization=OPENAI_ORGANIZATION)
 
 # 임베딩 함수 설정
 embedding = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
